56ebd35c69e43160993f1862e2f5175d4511471c
Matt Fellows <matt.fellows@onegeek.com.au>
2020-04-20 07:20:33 +1000

    chore: link pact package during example tests on Appveyor

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,9 +1,21 @@
 npm run dist
 
+cd dist
+npm link
+cd ..
+cd dist-web
+npm link
+cd ..
+
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
   cd $_.FullName
   npm i
+  If ($_.FullName -match "karma") {
+    npm link @pact-foundation/pact-web
+  } else {
+    npm link @pact-foundation/pact
+  }
   npm t
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
