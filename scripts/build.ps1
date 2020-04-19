e945006cff9f3eef0fa802c377bc2fbe8badb827
Matt Fellows <matt.fellows@onegeek.com.au>
2020-04-20 07:24:12 +1000

    chore: deprecate node 8 on appveyor

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,6 @@
 npm run dist
+cp package.json dist/package.json
+cp package.json.web dist-web/package.json
 
 cd dist
 npm link
@@ -6,16 +8,24 @@ cd ..
 cd dist-web
 npm link
 cd ..
+dir dist
+dir dist-web
 
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
   cd $_.FullName
   npm i
+
   If ($_.FullName -match "karma") {
+    Write-Output "Linking pact web"
     npm link @pact-foundation/pact-web
   } else {
+    Write-Output "Linking pact"
     npm link @pact-foundation/pact
   }
+
+  Write-Output "Running tests"
   npm t
+
   if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 }
