8b0e5d3e3ddd3a1c032c297b3520ddd9272477ba
Timothy Jones <tim.jones@versent.com.au>
2020-04-21 13:12:15 +1000

    chore(appveyor): Run npm link on windows builds

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,11 @@
 npm run dist
 
+cd dist
+npm link
+cd ..\dist-web
+npm link
+cd ..
+
 Get-ChildItem ".\examples" -Directory | ForEach-Object {
   Write-Output "Running examples in $($_.FullName)"
   cd $_.FullName
